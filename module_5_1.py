f835b2127da89d403ccebd3bbe8ac773eb40f3f0
AlexCo0711 <alexco@list.ru>
2024-09-06 14:28:48 +0300

    Update module_5_1.py
    Домашняя работа по уроку "Атрибуты и методы объекта."

--- module_5_1.py
+++ module_5_1.py
@@ -1,6 +1,6 @@
 # Домашняя работа по уроку "Атрибуты и методы объекта."
 # Дополнил код вводом произвольных исходных данных
-gk = input('введите имя дома: ')
+home = input('введите имя дома: ')
 maxfloor = input('введжите количество этажей дома: ')
 floor = input('введите номер необходимого этажа: ')
 # Объявление класса House
@@ -12,14 +12,14 @@ class House():
 
     # Создание метода go_to с параметром new_floor и
     # логикой внутри него на основе описания задачи
+    # и атрибутом new_floor для "плюшек"
     def go_to(self, new_floor):
         self.new_floor = new_floor
         # Если new_floor больше чем self.number_of_floors или меньше 1,
         # то вывод строки "Такого этажа не существует"
         if int(new_floor) > int(self.number_of_floors) or 1 > int(new_floor):
-            print(f'Такого этажа {self.new_floor} не существует в доме "{self.name}"')
-            print(f'В доме "{self.name}" не более {self.number_of_floors} этажей')
-        # вывод на экран (в консоль) значения от 1 до new_floor(включительно)
+            print(f'Такого этажа {self.new_floor} не существует в доме "{self.name}"\nВ доме "{self.name}" не более {self.number_of_floors} этажей')
+        # вывод на экран (в консоль) значения от 1 до new_floor (включительно)
         else:
             print(f'Этажи в доме "{self.name}":')
             for i in range(int(new_floor)):
@@ -32,6 +32,6 @@ h2 = House('Домик в деревне', 2)
 h1.go_to(5)
 h2.go_to(10)
 
-# Произвольные исходные данные согласно введеных ранее знаений
-home = House(gk, maxfloor)
-home.go_to(floor)
+# Произвольные исходные данные согласно введеных ранее значений
+hm = House(home, maxfloor)
+hm.go_to(floor)
